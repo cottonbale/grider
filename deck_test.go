949df7790bdcc33e8d02285e0b9cda4f96f3579e
cottonbale <cottonbale@gmail.com>
2020-05-25 14:05:44 +0100

    more tests

--- deck_test.go
+++ deck_test.go
@@ -8,4 +8,13 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected a deck length of 16 but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be ace of spades but got %v instead", d[0])
+	}
+
+	if d[len(d) - 1] != "Four of Clubs" {
+		t.Errorf("Expected last card to be four of clubs but got %v instead", d[len(d)-1])
+	}
+
 }

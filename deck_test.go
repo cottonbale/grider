4d2a4f1bfbeda35f8ff225762bfb9d69bebc2ba6
cottonbale <cottonbale@gmail.com>
2020-05-25 17:05:40 +0100

    last bit

--- deck_test.go
+++ deck_test.go
@@ -16,5 +16,21 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d) - 1] != "Four of Clubs" {
 		t.Errorf("Expected last card to be four of clubs but got %v instead", d[len(d)-1])
 	}
+}
 
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	//os.Remove("_decktesting")
+	os.Remove("_decktesting")
+
+	
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
